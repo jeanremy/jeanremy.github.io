4d1bfef47e55df857113b7de5f1f27714ea70e9d
JR <contact@jeanremypraud.com>
2022-11-02 23:23:37 +0100

    :art: Try using docs folder

--- .eleventy.js
+++ .eleventy.js
@@ -35,7 +35,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "_site",
+      output: "docs",
     },
     passthroughFileCopy: true,
   };
