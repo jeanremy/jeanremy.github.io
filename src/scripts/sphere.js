77808ebd0dd769ee532d515bd37111a361cab278
JR <contact@jeanremypraud.com>
2022-11-02 23:03:51 +0100

    :sparkles: Add Eleventy

--- src/scripts/sphere.js
+++
@@ -1,9 +0,0 @@
-import { SphereGeometry, MeshBasicMaterial, Mesh } from 'three/build/three.module'
-import { black, radius } from './utilities'
-
-// Sphere
-var geometry = new SphereGeometry(radius, 32, 32)
-var material = new MeshBasicMaterial({ color: black, transparent: true, opacity: 1 })
-var sphere = new Mesh(geometry, material)
-
-export default sphere
