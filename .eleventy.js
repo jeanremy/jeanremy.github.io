b2fa4092426fd1f61aacdfb6561173363e79e721
JR <contact@jeanremypraud.com>
2023-10-23 17:55:34 +0200

    :sparkles: Add robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -1,44 +1,45 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const faviconPlugin = require("eleventy-favicon");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation')
+const pluginRss = require('@11ty/eleventy-plugin-rss')
+const faviconPlugin = require('eleventy-favicon')
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 
-const { console, format, cssmin } = require("./src/_11ty/filters");
-const { posts } = require("./src/_11ty/collections");
-const { markdownit } = require("./src/_11ty/libraries");
+const { console, format, cssmin } = require('./src/_11ty/filters')
+const { posts } = require('./src/_11ty/collections')
+const { markdownit } = require('./src/_11ty/libraries')
 
 module.exports = function (eleventyConfig) {
   // ---------- PLUGINS --------------------
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(faviconPlugin);
-  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(eleventyNavigationPlugin)
+  eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.addPlugin(faviconPlugin)
+  eleventyConfig.addPlugin(syntaxHighlight)
 
   // ---------- PASSTHROUGH --------------------
-  eleventyConfig.addPassthroughCopy("src/assets/fonts");
-  eleventyConfig.addPassthroughCopy("src/assets/img");
-  eleventyConfig.addPassthroughCopy("CNAME");
-  eleventyConfig.addPassthroughCopy("index.opml");
+  eleventyConfig.addPassthroughCopy('src/assets/fonts')
+  eleventyConfig.addPassthroughCopy('src/assets/img')
+  eleventyConfig.addPassthroughCopy('CNAME')
+  eleventyConfig.addPassthroughCopy('robots.txt')
+  eleventyConfig.addPassthroughCopy('index.opml')
 
   // ---------- FILTERS --------------------
-  eleventyConfig.addFilter("cssmin", cssmin);
-  eleventyConfig.addFilter("format", format);
-  eleventyConfig.addFilter("console", console);
+  eleventyConfig.addFilter('cssmin', cssmin)
+  eleventyConfig.addFilter('format', format)
+  eleventyConfig.addFilter('console', console)
 
   // ---------- COLLECTIONS --------------------
-  eleventyConfig.ignores.add("**/_*.md");
-  eleventyConfig.addCollection("posts", posts);
+  eleventyConfig.ignores.add('**/_*.md')
+  eleventyConfig.addCollection('posts', posts)
 
-  eleventyConfig.setLibrary("md", markdownit(eleventyConfig));
+  eleventyConfig.setLibrary('md', markdownit(eleventyConfig))
 
   return {
     dir: {
-      input: "src",
-      output: "_site",
-      includes: "_includes",
-      layouts: "_layouts",
+      input: 'src',
+      output: '_site',
+      includes: '_includes',
+      layouts: '_layouts',
     },
 
     passthroughFileCopy: true,
-  };
-};
+  }
+}
