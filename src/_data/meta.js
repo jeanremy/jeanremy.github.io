1693ca4130808759c4a195df6c8516b1906edff0
JR <contact@jeanremypraud.com>
2024-07-08 17:07:09 +0200

    :art: Change meta desc

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,7 +2,7 @@ export default {
   siteName: 'Jean-Rémy Praud',
   siteBaseline: 'Developpeur fullstack',
   siteDescription:
-    'Tech Lead chez Dernier Cri, j’aime travailler sur des projets porteurs de sens. Je m’intéresse tout particulièrement à l’éco-conception et à l’accessibilité.',
+    'Developpeur fullstack, j’aime travailler sur des projets porteurs de sens. Je m’intéresse tout particulièrement à l’éco-conception et à l’accessibilité.',
   siteType: 'Person', // schema
   siteURL: 'https://jeanremypraud.com',
   locale: 'fr_FR',
